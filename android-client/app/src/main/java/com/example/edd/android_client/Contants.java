681f9731b2a5d3ee9c280b26af2aa777a9f1e33e
Edd <edwinkamaumuraya0@outlook.com>
2018-03-04 16:19:38 +0300

    added a constants.java file in the android project

---
+++ android-client/app/src/main/java/com/example/edd/android_client/Contants.java
@@ -0,0 +1,10 @@
+package com.example.edd.android_client;
+
+/**
+ * Created by edd on 3/4/2018.
+ */
+
+public class Contants {
+    private  static final String Root_url="http://localhost/v1/registeruser.php";
+    
+}
